a98bfaa91720a0ac6fda8e348643cb9ce9a709d7
Paul Traylor <kungfudiscomonkey@gmail.com>
2012-09-12 21:30:00 -0700

    Move the TeamCity specific code to its own module

---
+++ TeamcityCat/providers/teamcity.py
@@ -0,0 +1,76 @@
+import logging
+import requests
+import xml.etree.cElementTree as ET
+
+logger = logging.getLogger(__name__)
+
+
+class TeamCity(object):
+    def __init__(self, host):
+        self.host = host
+
+    def poll(self):
+        successes = []
+        investigation = []
+        failures = []
+
+        try:
+            req = requests.get(self.host)
+
+            if req.status_code is not 200:
+                raise IOError("Error loading feed")
+
+            content = """<?xml version="1.0" encoding="UTF-8"?><root>%s</root>""" % req.text.replace('&', '&amp;')
+            doc = ET.fromstring(content)
+
+            # Loop through each project
+            for table in doc.findall('table'):
+
+                group = None
+                for a in table.findall('tr/td/div/a'):
+                    if a.attrib.get('class') == 'buildTypeName':
+                        group = a.text
+                for tr in table.findall('tr'):
+                    entry = {
+                        'project': None,
+                        'build': None,
+                        'status': None,
+                        'group': group
+                    }
+
+                    # loop through each build config
+                    for td in tr.findall('td'):
+                        # Get success status
+                        if td.attrib.get('class') == 'buildConfigurationName':
+                            entry['project'] = td.find('a').text
+                            entry['status'] = td.find('img').attrib['title']
+                        if td.attrib.get('class') == 'buildNumberDate':
+                            entry['build'] = td.find('div/a').text
+
+                    # Skip project rows
+                    if entry['status'] is None:
+                        continue
+
+                    if 'success' in entry['status']:
+                        successes.append(entry)
+                    elif 'investigating' in entry['status']:
+                        investigation.append(entry)
+                    else:
+                        failures.append(entry)
+
+        except Exception, e:
+            logger.exception('Exception found while loading feeds')
+            failures.append({
+                'project': '?',
+                'build': e,
+                'group': '?',
+                })
+
+        for success in successes:
+            logger.info('Success: %(group)15s : %(project)-30s %(build)s' % success)
+        for warning in investigation:
+            logger.warning('Investigation: %(group)15s : %(project)-30s %(build)s' % warning)
+        for failure in failures:
+            logger.error('Failure: %(group)15s : %(project)-30s %(build)s' % failure)
+
+        return successes, investigation, failures
--- TeamcityCat/web.py
+++ TeamcityCat/web.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template
-import xml.etree.cElementTree as ET
 import argparse
 import time
-import requests
 import logging
 import glob
 import os
 from TeamcityCat import settings
+from providers.teamcity import TeamCity
 
 logger = logging.getLogger(__name__)
 app = Flask(__name__)
@@ -45,70 +44,11 @@ def find_cats():
 
 @app.route("/")
 def index():
-    successes = []
-    investigation = []
-    failures = []
+    tc = TeamCity(settings.TEAMCITY_WIDGET)
+
+    successes, investigation, failures = tc.poll()
 
     cats = find_cats()
-
-    try:
-        req = requests.get(settings.TEAMCITY_WIDGET)
-
-        if req.status_code is not 200:
-            raise IOError("Error loading feed")
-
-        content = """<?xml version="1.0" encoding="UTF-8"?><root>%s</root>""" % req.text.replace('&', '&amp;')
-        doc = ET.fromstring(content)
-
-        # Loop through each project
-        for table in doc.findall('table'):
-
-            group = None
-            for a in table.findall('tr/td/div/a'):
-                if a.attrib.get('class') == 'buildTypeName':
-                    group = a.text
-            for tr in table.findall('tr'):
-                entry = {
-                    'project': None,
-                    'build': None,
-                    'status': None,
-                    'group': group
-                }
-
-                # loop through each build config
-                for td in tr.findall('td'):
-                    # Get success status
-                    if td.attrib.get('class') == 'buildConfigurationName':
-                        entry['project'] = td.find('a').text
-                        entry['status'] = td.find('img').attrib['title']
-                    if td.attrib.get('class') == 'buildNumberDate':
-                        entry['build'] = td.find('div/a').text
-
-                # Skip project rows
-                if entry['status'] is None:
-                    continue
-
-                if 'success' in entry['status']:
-                    successes.append(entry)
-                elif 'investigating' in entry['status']:
-                    investigation.append(entry)
-                else:
-                    failures.append(entry)
-
-    except Exception, e:
-        logger.exception('Exception found while loading feeds')
-        failures.append({
-            'project': '?',
-            'build': e,
-            'group': '?',
-            })
-
-    for success in successes:
-        logging.info('Success: %(group)15s : %(project)-30s %(build)s' % success)
-    for warning in investigation:
-        logging.warning('Investigation: %(group)15s : %(project)-30s %(build)s' % warning)
-    for failure in failures:
-        logging.error('Failure: %(group)15s : %(project)-30s %(build)s' % failure)
 
     if len(failures) > 0:
         return render_template(
