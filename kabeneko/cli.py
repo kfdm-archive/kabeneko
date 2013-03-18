41df33a8b2dee3531adccf6464fd92cd7d4dd164
Paul Traylor <kungfudiscomonkey@gmail.com>
2013-03-17 21:43:52 -0700

    Allow configuring the image path

--- kabeneko/cli.py
+++ kabeneko/cli.py
@@ -1,7 +1,12 @@
 import argparse
 import logging
+import os
 
 from kabeneko.web import app
+
+
+ROOT_DIR = os.path.dirname(__file__)
+STATIC_DIR = os.path.join(ROOT_DIR, 'static')
 
 
 def main():
@@ -15,11 +20,18 @@ def main():
     )
     parser.add_argument('--host', default='127.0.0.1')
     parser.add_argument('--port', default=8000, type=int)
+    parser.add_argument(
+        '--images',
+        default=STATIC_DIR,
+        help='Path to static images (Defaults to packaged images)'
+    )
     options = parser.parse_args()
 
     options.verbosity = logging.WARNING - (options.verbosity * 10)
     # Set our starting logging level
     logging.basicConfig(level=options.verbosity)
+
+    app.config.options = options
 
     app.run(
         debug=(options.verbosity < logging.WARNING),
--- kabeneko/web.py
+++ kabeneko/web.py
@@ -1,16 +1,10 @@
 from flask import Flask, render_template
-import logging
 
-import os
 from kabeneko import settings
 from kabeneko.providers.teamcity import TeamCity
 from kabeneko.images import Cats
 
-logger = logging.getLogger(__name__)
 app = Flask(__name__)
-
-__root__ = os.path.dirname(__file__)
-__static__ = os.path.join(__root__, 'static')
 
 
 @app.route("/")
@@ -19,7 +13,7 @@ def index():
 
     successes, investigation, failures = tc.poll()
 
-    cats = Cats(__static__).images()
+    cats = Cats(app.config.options.images).images()
 
     if len(failures) > 0:
         return render_template(
@@ -39,7 +33,7 @@ def index():
 
 @app.route('/success')
 def success():
-    cats = Cats(__static__).images()
+    cats = Cats(app.config.options.images).images()
     return render_template('happy.html',
         cats=cats
         )
@@ -47,7 +41,7 @@ def success():
 
 @app.route('/worried')
 def worried():
-    cats = Cats(__static__).images()
+    cats = Cats(app.config.options.images).images()
     investigation = [{
             'group': 'Project Team',
             'project': 'Build Configuration',
@@ -63,7 +57,7 @@ def worried():
 
 @app.route('/fail')
 def fail():
-    cats = Cats(__static__).images()
+    cats = Cats(app.config.options.images).images()
     failures = [{
             'group': 'group',
             'project': 'project',
